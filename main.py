c88d4661cc69445212737aa9b65f73c7d9ad70fb
Noriyuki Hosaka <nori@asama.tonic-water.com>
2009-05-21 11:38:51 +0900

    added and pass unittests 	modified:   main.py 	modified:   test.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 
+import sys
 import urlparse
 import wsgiref.handlers
 
@@ -23,22 +24,81 @@ from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 
 cache_related = ('if_match', 'if_modified_since', 'if_none_match', 'if_range', 'if_unmodified_since')
-NETLOC = 'image.backgammonbase.com'
+PROHIBTED = ('Content-Length', )
+PROHIBTED_IN_ENTITYLESS = PROHIBTED + ('Content-Type', )
+ENTITYLESS_STATUS = (204, 205, 304)
+
+#NETLOC = 'image.backgammonbase.com'
+NETLOC = '192.168.2.64'
+HOST = 'image.backgammonbase.com'
 SCHEME = 'http'
 
+
 class ReverseProxyHandler(webapp.RequestHandler):
+  def relay_response(self, src, status):
+    assert status
+    self.response.set_status(status)
+    for key in src.headers:
+      self.response.headers[key] = src.headers[key]
+
+    if status in ENTITYLESS_STATUS:
+      for key in PROHIBTED_IN_ENTITYLESS:
+        del self.response.headers[key]
+    else:
+      self.response.out.write(src.content)
+      for key in PROHIBTED:
+        del self.response.headers[key]
+
   def get(self):
     u = self.request.url
     scheme, netloc, path, query, fragment = urlparse.urlsplit(u)
     t = urlparse.urlunsplit((SCHEME, NETLOC, path, query, fragment))
+    if_none_match = self.request.headers.get('If-None-Match', None)
     cached = memcache.get(u)
     if cached is None:
-      response = urlfetch.fetch(t)
-      expires = response.headers['Expires']
-      memcache.set(u, response)
-      cached = response
-    self.response.headers['Content-Type'] = cached.headers['Content-Type']
-    self.response.out.write(cached.content)
+      r = urlfetch.fetch(t, 
+            headers={'host': HOST}
+            )
+      assert r.status_code == 200
+      memcache.set(u, r)
+      self.relay_response(r, 200)
+      return
+    elif (if_none_match and
+          (cached.headers['etag'] == if_none_match)):
+      r = urlfetch.fetch(t, 
+          headers={
+#            'If-Modified-Since': cached.headers['date'],
+            'If-None-Match': cached.headers['etag'],
+            'host': HOST
+          }
+          )
+      if r.status_code == 200:
+        memcache.set(u, r)
+        self.relay_response(r, 200)
+        return
+      elif r.status_code == 304:
+        print 'got 304 from source'
+        if if_none_match is not None:
+          print 'got matching etag from client'
+          self.relay_response(r, status=304)
+        else:
+          print 'got no etag from client'
+          self.relay_response(cached, status=200)
+        return
+      else:
+        assert False
+    elif (if_none_match and 
+          (cached.headers['etag'] != if_none_match)):
+      r = urlfetch.fetch(t, 
+            headers={'host': HOST}
+            )
+      assert r.status_code == 200
+      memcache.set(u, r)
+      self.relay_response(r, 200)
+      return
+    else:
+      pass
+    assert False
 
 
 def main():
--- test.py
+++ test.py
@@ -3,24 +3,115 @@ import unittest
 from webtest import TestApp
 
 from google.appengine.ext import webapp
+from google.appengine.api import memcache
 from main import ReverseProxyHandler
 
 #class CCTest(unittest.TestCase):
 
 
-app = TestApp(webapp.WSGIApplication([('.*', ReverseProxyHandler)],
+class HandlerTest(unittest.TestCase):
+  def setUp(self):
+    self.app = TestApp(
+                   webapp.WSGIApplication([('.*', ReverseProxyHandler)],
                                        debug=True)
-                                       )
-#class HandlerTest(unittest.TestCase):
-#  pass
+                   )
+  def tearDown(self):
+    memcache.flush_all()
+    del self.app
 
-def test_contenttype():
-  response = app.get('/image'
+  def test_contenttype_png(self):
+    response = self.app.get(('/image'
                      '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
                      '&height=300'
                      '&width=400'
                      '&css=minimal'
-                     '&format=png')
-  assert response.headers['Content-Type'] == 'image/png'
+                     '&format=png'),
+                     status=200
+                     )
+    assert response.headers['Content-Type'] == 'image/png'
 
+  def test_contenttype_jpeg(self):
+    response = self.app.get(('/image'
+                     '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
+                     '&height=300'
+                     '&width=400'
+                     '&css=minimal'
+                     '&format=jpeg'),
+                     status=200
+                     )
+    assert response.headers['Content-Type'] == 'image/jpeg'
+
+  def test_status200(self):
+    response = self.app.get(('/image'
+                     '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
+                     '&height=300'
+                     '&width=400'
+                     '&css=minimal'
+                     '&format=png'),
+                     status=200
+                     )
+    if response.headers['Content-Type'].startswith('text/html'):
+      print response
+      #print dir(response)
+      print len(response.body)
+    print response.status
+    print response.headers
+    assert response.headers['Content-Type'] == 'image/png'
+
+  def test_etag_match(self):
+    response = self.app.get(('/image'
+                     '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
+                     '&height=300'
+                     '&width=400'
+                     '&css=minimal'
+                     '&format=png'),
+                     status=200
+                      )
+    print 'first response (populated memcache)'
+    print response.status
+    print response.headers
+    print response.headers['etag']
+    assert response.headers['Content-Type'] == 'image/png'
+    response = self.app.get(('/image'
+                     '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
+                     '&height=300'
+                     '&width=400'
+                     '&css=minimal'
+                     '&format=png'),
+                     headers={
+                        'If-None-Match': response.headers['etag'],
+                          },
+                     )
+    print 'second response'
+    print response.status
+    print response.headers
+    assert response.status.startswith('304')
+
+  def test_etag_mismatch(self):
+    response = self.app.get(('/image'
+                     '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
+                     '&height=300'
+                     '&width=400'
+                     '&css=minimal'
+                     '&format=png'),
+                     status=200
+                      )
+    print 'first response (populated memcache)'
+    print response.status
+    print response.headers
+    assert response.headers['Content-Type'] == 'image/png'
+    response = self.app.get(('/image'
+                     '?gnubgid=4HPwATDgc%2FABMA%3AMAAAAAAAAAAA'
+                     '&height=300'
+                     '&width=400'
+                     '&css=minimal'
+                     '&format=png'),
+                     headers={
+                        'If-None-Match': 'some_thing_invalid'
+                          },
+                     )
+    print 'second response'
+    print response.status
+    print response.headers
+    assert response.status.startswith('200')
 
