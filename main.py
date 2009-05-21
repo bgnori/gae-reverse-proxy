30819186692275af94c2c19621408ee5339c01ae
Noriyuki Hosaka <nori@asama.tonic-water.com>
2009-05-21 17:32:45 +0900

    added README for github changed PROHIBITED headers in GAE. 	new file:   README 	modified:   main.py

--- main.py
+++ main.py
@@ -24,11 +24,21 @@ from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 
 cache_related = ('if_match', 'if_modified_since', 'if_none_match', 'if_range', 'if_unmodified_since')
-PROHIBTED = ('Content-Length', )
-PROHIBTED_IN_ENTITYLESS = PROHIBTED + ('Content-Type', )
+
+'''
+http://code.google.com/intl/ja/appengine/docs/webapp/responseclass.html#Disallowed_HTTP_Response_Headers
+'''
+PROHIBTED_BY_GAE = ('Content-Encoding', 
+                    'Content-Length', 
+                    'Date',
+                    'Server',
+                    'Transfer-Encoding')
+
+PROHIBTED_IN_ENTITYLESS = PROHIBTED_BY_GAE + ('Content-Type', )
 ENTITYLESS_STATUS = (204, 205, 304)
 
 #NETLOC = 'image.backgammonbase.com'
+#NETLOC = '127.0.0.1'
 NETLOC = '192.168.2.64'
 HOST = 'image.backgammonbase.com'
 SCHEME = 'http'
@@ -46,7 +56,7 @@ class ReverseProxyHandler(webapp.RequestHandler):
         del self.response.headers[key]
     else:
       self.response.out.write(src.content)
-      for key in PROHIBTED:
+      for key in PROHIBTED_BY_GAE:
         del self.response.headers[key]
 
   def get(self):
@@ -77,12 +87,9 @@ class ReverseProxyHandler(webapp.RequestHandler):
         self.relay_response(r, 200)
         return
       elif r.status_code == 304:
-        print 'got 304 from source'
         if if_none_match is not None:
-          print 'got matching etag from client'
           self.relay_response(r, status=304)
         else:
-          print 'got no etag from client'
           self.relay_response(cached, status=200)
         return
       else:
