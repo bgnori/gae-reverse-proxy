b21bb7151455c687df614420c2c02177cdf85391
Noryuki Hosaka <nori@shinano.tonic-water.com>
2009-09-15 12:27:10 +0900

    added save/load method to Mapping 	modified:   lib/dsar.py

--- lib/dsar.py
+++ lib/dsar.py
@@ -25,16 +25,21 @@ class PropertyMeta(type):
 
 class PropertyBase(object):
   __metaclass__ = PropertyMeta
-  def __init__(self, p):
-    self.table, self.col = p
+  def __init__(self, col=None):
+    self.col = col
+
   @classmethod
   def python_type(cls):
     return None
   @classmethod
   def sqlite_type(cls):
     return "NULL"
-  def to_sqlite(self):
+  @classmethod
+  def to_sqlite(cls):
     return None
+  @classmethod
+  def to_python(cls, v):
+    return cls.python_type()(v)
 
 
 class KeyCol(PropertyBase):
@@ -44,8 +49,6 @@ class KeyCol(PropertyBase):
   @classmethod
   def sqlite_type(cls):
     return "INTEGER"
-  def to_sqlite(self):
-    return None
 
 
 class StringCol(PropertyBase):
@@ -55,24 +58,24 @@ class StringCol(PropertyBase):
   @classmethod
   def sqlite_type(cls):
     return "TEXT"
-  def to_sqlite(self):
-    return None
 
   
 class ReferenceCol(PropertyBase):
-  def __init__(self, p, source):
-    PropertyBase.__init__(self, p)
-    self.src_table, self.src_col = source
+  def __init__(self, source, *args, **kw):
+    PropertyBase.__init__(self, *args, **kw)
+    self.ref = source
+
   @classmethod
   def python_type(cls):
-    return int
+    #UGH!  Lazy bind this.
+    return self.ref
+
   @classmethod
   def sqlite_type(cls):
     return "INTEGER"
-  def to_sqlite(self):
-    return None
 
-def write(*objs, conn):
+
+def write(conn=None, *objs):
   for o in objs:
     o.save(conn)
 
@@ -80,16 +83,7 @@ def write(*objs, conn):
 
 class MappingMeta(type):
   def __new__(cls, name, bases, dct):
-    klass = type.__new__(cls, name, bases, dct)
-    def init(self, *args, **kws):
-      print 'init', cls, name
-      for key, prop in dct.items():
-        if isinstance(prop, PropertyBase):
-          if key in kws:
-            print key, prop, 'with', kws[key]
-            self.__dict__[key] = prop.python_type()(kws[key])
-    klass.__init__ = init
-    return klass
+    return type.__new__(cls, name, bases, dct)
 
   def __init__(cls, name, bases, dct):
     super(MappingMeta, cls).__init__(name, bases, dct)
@@ -98,58 +92,85 @@ class MappingMeta(type):
 
 class Mapping:
   __metaclass__ = MappingMeta
-  def save(self, connection):
-    c = connection.cursor()
+  def __init__(self, *args, **kws):
     for key, prop in self.__class__.__dict__.items():
       if isinstance(prop, PropertyBase):
-        c.execute("insert into %s(%s) values (?)"%(prop.table, prop.col),
-                  (getattr(self, key),))
-  def load(self, connection):
-    pass
+        if key in kws:
+          print key, prop, 'with', kws[key]
+          self.__dict__[key] = prop.python_type()(kws[key])
+
+  @classmethod
+  def table(cls):
+    return cls.__name__
+
+  @classmethod
+  def columns(cls):
+    return [(prop.col or key, prop.sqlite_type() ) for key, prop in cls.__dict__.items() if isinstance(prop, PropertyBase)]
+
+  @classmethod
+  def new_schema(cls, connection):
+    c = connection.cursor()
+    vs = ', '.join(['%s %s'%(k, t) for k, t in cls.columns()])
+    q = "create table %s(%s)"%(cls.table(), vs)
+    print q
+    c.execute(q)
+
+  def save(self, connection):
+    c = connection.cursor()
+    cs = self.columns()
+    ks = ', '.join(['%s'%(k,) for k, t in cs])
+    q = "insert into %s(%s) values (%s)"%(self.table(), ks, ', '.join('?'*len(cs)))
+    print q
+    c.execute(q, [getattr(self, k) for k, t in cs])
+
+  @classmethod
+  def load(cls, connection, key):
+    c = connection.cursor()
+    cs = cls.columns()
+    ks = ', '.join(['%s'%(k,) for k, t in cs])
+    q = "select %s from %s"%(ks, cls.table())
+    print q
+    c.execute(q)
+
+    r = cls()
+    for i, v in  enumerate(c.fetchone()):
+      k, t = cs[i]
+      p = getattr(cls, k)
+      setattr(r, k, p.to_python(v))
+    return r
+
+
+
 
 class WebSite(Mapping):
-  key = KeyCol(('website', 'id'))
-  name = StringCol(('website', 'name'))
+  key = KeyCol()
+  name = StringCol()
 
 class Domain(Mapping):
-  key = KeyCol(('domain','id'))
-  domain = StringCol(('domain', 'name'))
-  website = ReferenceCol(('domain', 'website'), ('website', 'id'))
+  key = KeyCol()
+  domain = StringCol()
+  website = ReferenceCol(WebSite)
 
 class URL(Mapping):
-  key = KeyCol(('url', 'id'))
-  url = StringCol(('url', 'data'))
-  domain = ReferenceCol(('url', 'domain'), ('domain', 'id'))
+  key = KeyCol()
+  url = StringCol()
+  domain = ReferenceCol(Domain)
 
 #conn = sqlite3.connect(os.path.join(dir, site['name']))
 
 if __name__ == "__main__":
+    print WebSite.table()
+    print WebSite.columns()
     conn = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES)
-    cur = conn.cursor()
-    cur.execute("create table website(id INTEGER, name TEXT)")
+
+    WebSite.new_schema(conn)
     w = WebSite(key=1, name="hoge")
     w.save(conn)
-    cur.execute("select name from website")
-    obj = cur.fetchone()[0]
-    print obj
-    print type(obj)
+    x = WebSite.load(conn, key=1)
+    print x
+    print x.key
+    print x.name
 
-    '''
-    cur.execute("create table domain(X Domain)")
-    cur.execute("insert into domain(X) values (?)", (Domain("hoge"),))
-    cur.execute("select X from domain")
-    obj = cur.fetchone()[0]
-    print obj
-    print type(obj)
-
-    cur.execute("create table url(X URL)")
-    cur.execute("insert into url(X) values (?)", (URL("hoge"),))
-    cur.execute("select X from url")
-    obj = cur.fetchone()[0]
-    print obj
-    print type(obj)
-    '''
-    cur.close()
     conn.close()
 
 
